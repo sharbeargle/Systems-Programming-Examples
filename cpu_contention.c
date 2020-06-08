52dd953b4e20e5b0a5160d7edbe9bf830420e078
vincehgov <vincehgov@gmail.com>
2020-06-07 17:23:20 -0700

    Implement cpu_contention with an infinite loop

--- cpu_contention.c
+++ cpu_contention.c
@@ -1,7 +1,23 @@
+/**
+ * Simulate CPU bound process by infinite loop
+ */
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int main() {
-    
+void do_stuff() {
+    int n = 1;
+    while (n < 2000000000) {
+        n = n + 1;
+    }
+    sleep(1);
+}
+
+int main(int argc, char** argv) {
+
+    while (1) {
+        do_stuff();
+    }
+
     return 0;
 }
